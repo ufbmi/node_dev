bf8249552cc592d4aec6363f139916dfd060ea80
Orta Santana <d.orta.santana@fctr-3217-m1.local>
2017-06-08 08:36:37 -0400

    refactored index page and main css

--- js/main.js
+++ js/main.js
@@ -1,90 +1,3 @@
-// // // var width = window.innerWidth;
-// // // var height = window.innerHeight;
-// // // var stage = new Konva.Stage({
-// // //     container: 'container',
-// // //     width: width,
-// // //     height: height
-// // // });
-// // // var layer = new Konva.Layer();
-
-// // // var tween = null;
-
-// // // function addStar(layer, stage) {
-// // //     var scale = Math.random();
-
-// // //     var star = new Konva.Star({
-// // //         x: Math.random() * stage.getWidth(),
-// // //         y: Math.random() * stage.getHeight(),
-// // //         numPoints: 5,
-// // //         innerRadius: 30,
-// // //         outerRadius: 50,
-// // //         fill: '#89b717',
-// // //         opacity: 0.8,
-// // //         draggable: true,
-// // //         scale: {
-// // //             x : scale,
-// // //             y : scale
-// // //         },
-// // //         rotation: Math.random() * 180,
-// // //         shadowColor: 'black',
-// // //         shadowBlur: 10,
-// // //         shadowOffset: {
-// // //             x : 5,
-// // //             y : 5
-// // //         },
-// // //         shadowOpacity: 0.6,
-// // //         // custom attribute
-// // //         startScale: scale
-// // //     });
-
-// // //     layer.add(star);
-// // // }
-
-// // // var dragLayer = new Konva.Layer();
-
-// // //  for(var n = 0; n < 3; n++) {
-// // //     addStar(layer, stage);
-// // // }
-
-// // // stage.add(layer, dragLayer);
-
-// // // //VERY USEFUL FOR DRAGGING RECOGNIZING
-
-// // // stage.on('dragstart', function(evt) {
-// // //     var shape = evt.target;
-// // //     // moving to another layer will improve dragging performance
-// // //     shape.moveTo(dragLayer);
-// // //     stage.draw();
-
-// // //     if (tween) {
-// // //         tween.pause();
-// // //     }
-// // //     shape.setAttrs({
-// // //         shadowOffset: {
-// // //             x: 15,
-// // //             y: 15
-// // //         },
-// // //         scale: {
-// // //             x: shape.getAttr('startScale') * 1.2,
-// // //             y: shape.getAttr('startScale') * 1.2
-// // //         }
-// // //     });
-// // // });
-
-// // // stage.on('dragend', function(evt) {
-// // //     var shape = evt.target;
-// // //     shape.moveTo(layer);
-// // //     stage.draw();
-// // //     shape.to({
-// // //         duration: 0.5,
-// // //         easing: Konva.Easings.ElasticEaseOut,
-// // //         scaleX: shape.getAttr('startScale'),
-// // //         scaleY: shape.getAttr('startScale'),
-// // //         shadowOffsetX: 5,
-// // //         shadowOffsetY: 5
-// // //     });
-// // // });
-
 /*Smooth scroll function*/
 $('#smooth').on('click', function(e) {
 
@@ -145,12 +58,7 @@ function scrollTopSmooth()
     }
 }
 
-
-
-
 // pop up func
-
-
 function popFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
@@ -163,13 +71,13 @@ function topFunction() {
     document.documentElement.scrollTop = 0; // For IE and Firefox
 }
 
-// curser functino
+// cursor function
 $(document).ready(function() {
     setInterval ('cursorAnimation0()', 1500);
 });
 
 function cursorAnimation0() {
-    $('#curser').animate({
+    $('#heart').animate({
         opacity: 0
     }, 'fast', 'swing').animate({
         opacity: 1
@@ -177,49 +85,47 @@ function cursorAnimation0() {
 }
 
 
+var width = window.innerWidth;
+var height = window.innerHeight;
+
+var stage = new Konva.Stage({
+    container: 'container',
+    width: width,
+    height: height
+});
 
 
-    var width = window.innerWidth;
-    var height = window.innerHeight;
+var layer = new Konva.Layer();
+/*
+* leave center point positioned
+* at the default which is at the center
+* of the hexagon
+*/
+var imageObj = new Image();
+var blueHex = new Konva.Image({
+    x: stage.getWidth() / 3,
+    y: stage.getHeight() / 4,
+    image: imageObj,
+    draggable: true
+});
 
+/*
+* move center point to right side
+* of hexagon
+*/
 
-    var stage = new Konva.Stage({
-        container: 'container',
-        width: width,
-        height: height
-    });
+layer.add(blueHex);
+stage.add(layer);
+var period = 2000;
 
-    
-    var layer = new Konva.Layer();
-    /*
-    * leave center point positioned
-    * at the default which is at the center
-    * of the hexagon
-    */
-    var imageObj = new Image();
-    var blueHex = new Konva.Image({
-        x: stage.getWidth() / 3,
-        y: stage.getHeight() / 4,
-        image: imageObj,
-        draggable: true
-    });
-   
-    /*
-    * move center point to right side
-    * of hexagon
-    */
-   
-    layer.add(blueHex);
-    stage.add(layer);
-    var period = 2000;
-    var anim = new Konva.Animation(function(frame) {
-        var scale = (8+Math.sin(frame.time * 2 * Math.PI / period))/40;
-        // scale x and y
-        blueHex.scale({ x :scale, y : scale});
+var anim = new Konva.Animation(function(frame) {
+    var scale = (8+Math.sin(frame.time * 2 * Math.PI / period))/40;
+    // scale x and y
+    blueHex.scale({ x :scale, y : scale});
 
-    }, layer);
-    imageObj.src = 'https://images.vexels.com/media/users/3/134121/isolated/preview/5ff73adb05d7f1fe47dd49bb1b08affa-star-cartoon-icon-50-by-vexels.png';
-    anim.start();
+}, layer);
+imageObj.src = 'https://images.vexels.com/media/users/3/134121/isolated/preview/5ff73adb05d7f1fe47dd49bb1b08affa-star-cartoon-icon-50-by-vexels.png';
+anim.start();
 
 
 //     var width = window.innerWidth;
@@ -401,5 +307,3 @@ function cursorAnimation0() {
 //         giraffe_black: 'giraffe-black.png'
 //     };
 //     loadImages(sources, initStage);
-
-
