e64582272f8b7a7b1cdbdda408319b6634d38fb9
yotoo2920 <d.orta.santana@ufl.edu>
2017-06-26 23:06:18 -0400

    WIP multL not close yet

--- js/our_languages.js
+++ js/our_languages.js
@@ -2,5 +2,12 @@
  * Created by yotoo2920 on 6/26/2017.
  */
 var multilanguage = {
-
-}
+    'en' : {
+        'websiteName' : 'Diabetes Tutorial For Kids',
+        'message' : 'To help you learn it, fight it and be strong about it'
+    },
+    'es' : {
+        'websiteName' : 'Tutorial para niños sobre Diabetes',
+        'message' : 'Para ayudarte a aprender y para que te enfrentes fuertemente contra ella'
+    }
+};
