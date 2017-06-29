5204de71faa0cfa8f31155d2afc34e2db7bfcb77
yotoo2920 <d.orta.santana@ufl.edu>
2017-06-29 17:17:56 -0400

    Chapter 1 looking good up to slide 5

--- js/main.js
+++ js/main.js
@@ -52,11 +52,11 @@ function scrollTopSmooth()
     }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
-    document.documentElement.scrollTop = 0; // For IE and Firefox
-}
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//     document.body.scrollTop = 0; // For Chrome, Safari and Opera
+//     document.documentElement.scrollTop = 0; // For IE and Firefox
+// }
 
 // Method for checking the answers in chapter 1
 document.getElementById("answer").onclick = validate;    
@@ -106,51 +106,51 @@ function cursorAnimation0() {
 }
 
 
-// -------------------------------------------------Konva lib objs--------------------//
+// // -------------------------------------------------Konva lib objs--------------------//
+//
+// var width = 120;
+// var height = 120;
+//
+// var stage = new Konva.Stage({
+//     container: ".resizingButton",
+//     width: width,
+//     height: height
+// });
+//
+//
+// var layer = new Konva.Layer();
+// /*
+// * leave center point positioned
+// * at the default which is at the center
+// * of the hexagon
+// */
+// var imageObj = new Image();
+// var blueHex = new Konva.Image({
+//     x: 0,
+//     y: 0,
+//     image: imageObj,
+//     draggable: false
+// });
+//
+// /*
+// * move center point to right side
+// * of hexagon
+// */
+//
+// layer.add(blueHex);
+// stage.add(layer);
+// var period = 2000;
+//
+// var anim = new Konva.Animation(function(frame) {
+//     var scale = (8+Math.sin(frame.time * 2 * Math.PI / period))/40;
+//     // scale x and y
+//     blueHex.scale({ x :scale, y : scale});
+//
+// }, layer);
+// imageObj.src = 'https://images.vexels.com/media/users/3/134121/isolated/preview/5ff73adb05d7f1fe47dd49bb1b08affa-star-cartoon-icon-50-by-vexels.png';
+// anim.start();
 
-var width = 120;
-var height = 120;
-
-var stage = new Konva.Stage({
-    container: ".resizingButton",
-    width: width,
-    height: height
-});
-
-
-var layer = new Konva.Layer();
-/*
-* leave center point positioned
-* at the default which is at the center
-* of the hexagon
-*/
-var imageObj = new Image();
-var blueHex = new Konva.Image({
-    x: 0,
-    y: 0,
-    image: imageObj,
-    draggable: false
-});
-
-/*
-* move center point to right side
-* of hexagon
-*/
-
-layer.add(blueHex);
-stage.add(layer);
-var period = 2000;
-
-var anim = new Konva.Animation(function(frame) {
-    var scale = (8+Math.sin(frame.time * 2 * Math.PI / period))/40;
-    // scale x and y
-    blueHex.scale({ x :scale, y : scale});
-
-}, layer);
-imageObj.src = 'https://images.vexels.com/media/users/3/134121/isolated/preview/5ff73adb05d7f1fe47dd49bb1b08affa-star-cartoon-icon-50-by-vexels.png';
-anim.start();
-
-// to load the keTones page into the iframe slide 7 chapter 1
-$(function(){
-    $("#ketonesWp").load("ch1Ketones/site/index.html");
-});
+// // to load the keTones page into the iframe slide 7 chapter 1
+// $(function(){
+//     $("#ketonesWp").load("ch1Ketones/site/index.html");
+// });
