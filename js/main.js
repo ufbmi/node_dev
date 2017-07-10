31e355a3c52ede06d2eeeac8663654996a4db06a
yotoo2920 <d.orta.santana@ufl.edu>
2017-07-10 09:01:03 -0400

    worked on chapter 1 for ipad

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+// beating heart function
+$(document).ready(function() {
+    setInterval ('cursorAnimation0()', 1800);
+});
+
+function cursorAnimation0() {
+    $('.beating').animate({
+        opacity: 0
+    }, 'fast', 'swing').animate({
+        opacity: 1
+    }, 'fast', 'swing');
+}
+
 /*Smooth scroll function*/
 $('#smooth').on('click', function(e) {
 
@@ -91,66 +104,3 @@ function validate() {
 
     return false;
 }
-
-// cursor function
-$(document).ready(function() {
-    setInterval ('cursorAnimation0()', 1500);
-});
-
-function cursorAnimation0() {
-    $('#heart').animate({
-        opacity: 0
-    }, 'fast', 'swing').animate({
-        opacity: 1
-    }, 'fast', 'swing');
-}
-
-
-// // -------------------------------------------------Konva lib objs--------------------//
-//
-// var width = 120;
-// var height = 120;
-//
-// var stage = new Konva.Stage({
-//     container: ".resizingButton",
-//     width: width,
-//     height: height
-// });
-//
-//
-// var layer = new Konva.Layer();
-// /*
-// * leave center point positioned
-// * at the default which is at the center
-// * of the hexagon
-// */
-// var imageObj = new Image();
-// var blueHex = new Konva.Image({
-//     x: 0,
-//     y: 0,
-//     image: imageObj,
-//     draggable: false
-// });
-//
-// /*
-// * move center point to right side
-// * of hexagon
-// */
-//
-// layer.add(blueHex);
-// stage.add(layer);
-// var period = 2000;
-//
-// var anim = new Konva.Animation(function(frame) {
-//     var scale = (8+Math.sin(frame.time * 2 * Math.PI / period))/40;
-//     // scale x and y
-//     blueHex.scale({ x :scale, y : scale});
-//
-// }, layer);
-// imageObj.src = 'https://images.vexels.com/media/users/3/134121/isolated/preview/5ff73adb05d7f1fe47dd49bb1b08affa-star-cartoon-icon-50-by-vexels.png';
-// anim.start();
-
-// // to load the keTones page into the iframe slide 7 chapter 1
-// $(function(){
-//     $("#ketonesWp").load("ch1Ketones/site/index.html");
-// });
